a3903fdee5cb19ae1f2671f7a1cb4347704d7ed4
Daniel Rubin <danielrubin95@hotmail.com>
2016-12-02 01:23:48 -0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -8,10 +8,9 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(30) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `key` varchar(32) NOT NULL,
   `online` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `users` (`id`, `user`, `password`, `key`, `online`) VALUES
-(1,	'user',	'1a1dc91c907325c69271ddf0c944bc72',	'minha-personal-key',	0);
+INSERT INTO `users` (`id`, `user`, `password`, `online`) VALUES
+(1,	'user',	'1a1dc91c907325c69271ddf0c944bc72',	0);
